a6016040f8fac870e099cfd033119c90868a7164
evmliaa-blip <yyagj2005@gmail.com>
2026-06-15 13:57:21 +0700

    delete and attention

--- backend/routes/api/protected/meal-plan.php
+++ backend/routes/api/protected/meal-plan.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('user')->middleware(EnsureActiveMembership::class)->group(function () {
     Route::get('/meal_plan', [MealPlanController::class, 'getMealPlan']);
     Route::put('/meal_plan', [MealPlanController::class, 'saveMealPlan']);
+    Route::delete('/meal_plan', [MealPlanController::class, 'deleteMealPlan']);
     Route::get('/food_log', [MealPlanController::class, 'getFoodLog']);
     Route::post('/food_log', [MealPlanController::class, 'addFood']);
     Route::delete('/food_log/{id}', [MealPlanController::class, 'deleteFood']);
